40481d4cd57ad8a93001fc5cbf78669d42aa0cd9
BrunonSocha <brunon.socha@gmail.com>
2025-01-16 17:41:15 +0100

    Mapy-2

---
+++ Simulator/Maps/BigMap.cs
@@ -0,0 +1,12 @@
+﻿namespace Simulator.Maps;
+
+internal class BigMap : Map
+{
+    public SmallMap(int sizeX, int sizeY) : base(sizeX, sizeY)
+    {
+        if (sizeX > 1000 || sizeY > 1000)
+        {
+            throw new ArgumentOutOfRangeException("The map is too big for a small map - the limit is 20.");
+        }
+    }
+}
---
+++ Simulator/Maps/BigTorusMap.cs
@@ -0,0 +1,28 @@
+﻿namespace Simulator.Maps;
+
+internal class BigTorusMap : BigMap
+{
+    public BigTorusMap(int sizeX, int sizeY) : base(sizeX, sizeY)
+    {
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+
+        int newX = ((nextPoint.X - 1 + SizeX) % SizeX) + 1;
+        int newY = ((nextPoint.Y - 1 + SizeY) % SizeY) + 1;
+
+        return new Point(newX, newY);
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextPoint = p.NextDiagonal(d);
+
+        int newX = ((nextPoint.X - 1 + SizeX) % SizeX) + 1;
+        int newY = ((nextPoint.Y - 1 + SizeY) % SizeY) + 1;
+
+        return new Point(newX, newY);
+    }
+}
